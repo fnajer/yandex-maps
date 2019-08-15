02d1ed8ed3d65593b1924da4a216400872f6e6c6
fnajer <lexactr@gmail.com>
2019-08-15 16:28:46 +0300

    create App component

---
+++ src/modules/App/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap'
+
+import MarkerInput from '../MarkerInput';
+import ListMarkers from '../ListMarkers';
+import Map from '../Map';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Container as="main">
+        <Row as="section" className="justify-content-center">
+          <Col xs={12} md={5} className="ml-auto mr-auto mb-3">
+            <MarkerInput />
+            <ListMarkers />
+          </Col>
+          <Col xs={12} md={7} className="d-flex justify-content-center">
+            <Map />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+
+export default App;
